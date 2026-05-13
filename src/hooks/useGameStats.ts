db29f1b55903ae5c1192f222832881f94003452e
farelod <411082779@qq.com>
2026-05-13 17:42:00 +0800

    新提交

--- src/hooks/useGameStats.ts
+++ src/hooks/useGameStats.ts
@@ -1,24 +1,18 @@
 import { useState, useEffect, useCallback } from 'react';
 
-export const useGameStats = (rowsCleared: number) => {
+export const useGameStats = () => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
   const linePoints = [40, 100, 300, 1200];
 
-  const calcScore = useCallback(() => {
-    // We have score
+  const updateStats = useCallback((rowsCleared: number) => {
     if (rowsCleared > 0) {
-      // This is how original Tetris score is calculated
       setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
       setRows(prev => prev + rowsCleared);
     }
-  }, [level, linePoints, rowsCleared]);
+  }, [level]);
 
-  useEffect(() => {
-    calcScore();
-  }, [calcScore, rowsCleared]);
-
-  return { score, setScore, rows, setRows, level, setLevel };
+  return { score, setScore, rows, setRows, level, setLevel, updateStats };
 };
